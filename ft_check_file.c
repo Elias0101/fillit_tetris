05f918eb56c1691fadaa15b7fce163ae9e6918d5
Scary Manhack <smanhack@at-j3.21-school.ru>
2019-04-22 14:26:32 +0300

    Fix

--- ft_check_file.c
+++ ft_check_file.c
@@ -6,7 +6,7 @@
 /*   By: smanhack <smanhack@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 11:50:13 by smanhack          #+#    #+#             */
-/*   Updated: 2019/04/19 18:54:09 by smanhack         ###   ########.fr       */
+/*   Updated: 2019/04/22 14:26:20 by smanhack         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,25 @@ static char	*ft_check_line(char *line)
 	return (ft_strdup(line));
 }
 
-static char	ft_defin_term(char *terms)
+static char	ft_defin_term(char **term)
 {
+	size_t i;
+	size_t j;
 	// Проверка на фигуры и определение их
-	terms = 0;
+	term = 0;
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			//if (term[i][j] == '#' && !(term[i + 1][j] == '#' || term[i][j + 1]
+			//== '#' || term[i - 1][j] == '#' || term[i][j - 1] == '#'))
+			//	return (0);
+			j++;
+		}
+		i++;
+	}
 	return (1);
 }
 
@@ -40,7 +55,7 @@ int			ft_check_file(int fd)
 	size_t	num_term;
 	size_t	num_str_term;
 	char	*term[4];
-	char	terms[100];
+	char	terms[26];
 
 	num_str = 0;
 	while (get_next_line(fd, &line) && (num_term = num_str / 5) < 26)
@@ -49,12 +64,12 @@ int			ft_check_file(int fd)
 		ft_putnbr(num_str);
 		ft_putnbr(num_str_term);
 		ft_putendl(line);
+		//ft_putendl("s");
 		if (num_str_term == 4)
 		{
-			//ft_putendl(line);
-			//if (line != NULL)// Проверка на пустую 5 строку
-			//	return (-1);
-			if ((terms[num_term] = ft_defin_term(terms)) == 0)
+			if ((*line) != '\0')// Проверка на пустую 5 строку
+				return (-1);
+			if ((terms[num_term] = ft_defin_term(term)) == 0)
 				return (-2);
 		}
 		else
@@ -68,7 +83,7 @@ int			ft_check_file(int fd)
 		line = NULL;
 		num_str++;
 	}
-	if (get_next_line(fd, &line) != 0 || num_term >= 26)
-		return (-5);
+	//if (get_next_line(fd, &line) != 0 || num_term >= 26 || line != NULL)
+	//	return (-5); //Проверка на последнюю строку
 	return (0);
 }
